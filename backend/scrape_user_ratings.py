0655e825c0f1d93f796db74cb720ae89af242d57
Samuel Wu <cernunnos1710@gmail.com>
2024-08-24 22:29:14 +0100

    Use lxml instead of beautifulsoup for speedy parsing; now runs less than 1s

--- backend/scrape_user_ratings.py
+++ backend/scrape_user_ratings.py
@@ -1,6 +1,6 @@
 import asyncio
 import aiohttp
-from bs4 import BeautifulSoup
+from lxml import html
 
 async def scrape_user_ratings(username: str):
     # Inner function fetching page content from a specified URL
@@ -12,14 +12,13 @@ async def scrape_user_ratings(username: str):
 
     # Use async sessions to make HTTP requests
     async with aiohttp.ClientSession() as session:
-        soup = BeautifulSoup(await _fetch_page(session, base_url), "lxml")
-        body = soup.find("body")
-        col_main_section = body.find("section", attrs={"col-main"})
-        num_pages_tag = col_main_section.select_one(
-            "div.pagination li.paginate-page:last-child a"
-        )
+        first_page_html = await _fetch_page(session, base_url)
+        root_tree = html.fromstring(first_page_html)
 
-        num_pages = int(num_pages_tag.text.strip()) if num_pages_tag else 1
+        if num_pages_res := root_tree.xpath("//div[@class='pagination']//li[@class='paginate-page']/a/text()"):
+            num_pages = int(num_pages_res[-1])
+        else:
+            num_pages = 1
         urls = [base_url] + [f"{base_url}/page/{page_num}"
                              for page_num in range(2, num_pages + 1)]
 
@@ -28,17 +27,18 @@ async def scrape_user_ratings(username: str):
 
         film_data = []
         for page in pages:
-            first_page = BeautifulSoup(page, "lxml")
-            containers = first_page.find_all("li", class_="poster-container")
+            tree = html.fromstring(page)
+            containers = tree.xpath("//li[@class='poster-container']")
             for container in containers:
-                film_name = container.select_one("div.linked-film-poster")["data-film-slug"]
-                liked = container.select_one("span.like") is not None
+                film_name = container.xpath(".//div[@class='linked-film-poster']/@data-film-slug")
+                liked = bool(container.xpath(".//span[contains(@class, 'like')]"))
 
+                # Check if a rating exists and fetch the rating if there is one
                 rating = None
-                if rating_tag := container.find("span", class_="rating"):
-                    rating_class = rating_tag["class"]
-                    rating = next((c for c in rating_class
-                                   if c.startswith("rated-")), None)
+                has_rating = container.xpath(".//span[contains(@class, 'rating')]/@class")
+                if has_rating:
+                    has_rating = has_rating[0].split()
+                    rating = next((c for c in has_rating if c.startswith("rated-")), None)
                     rating = rating.split("-")[1] if rating else None
 
                 film_data.append({
