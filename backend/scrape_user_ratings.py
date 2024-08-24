0b0e21dfcb8d9b3183174ce34d1d85593450c9fb
Samuel Wu <cernunnos1710@gmail.com>
2024-08-24 22:05:52 +0100

    Use asyncio for concurrency and bypass the glorious GIL. Scraper now running at 1.2-1.3s

--- backend/scrape_user_ratings.py
+++ backend/scrape_user_ratings.py
@@ -1,47 +1,50 @@
+import asyncio
+import aiohttp
 from bs4 import BeautifulSoup
-import requests
-import concurrent.futures
 
-def scrape_user_ratings(username: str):
-    url = f"https://letterboxd.com/{username}/films/by/date"
+async def scrape_user_ratings(username: str):
+    # Inner function fetching page content from a specified URL
+    async def _fetch_page(session, url):
+        async with session.get(url) as response:
+            return await response.text()
 
-    soup = BeautifulSoup(requests.get(url).text, "lxml")
-    body = soup.find("body")
+    base_url = f"https://letterboxd.com/{username}/films/by/date"
 
-    col_main_section = body.find("section", attrs={"col-main"})
-    num_pages = int(col_main_section.select_one("div.pagination li.paginate-page:last-child a").text.strip())
+    # Use async sessions to make HTTP requests
+    async with aiohttp.ClientSession() as session:
+        soup = BeautifulSoup(await _fetch_page(session, base_url), "lxml")
+        body = soup.find("body")
+        col_main_section = body.find("section", attrs={"col-main"})
+        num_pages_tag = col_main_section.select_one(
+            "div.pagination li.paginate-page:last-child a"
+        )
 
-    def _fetch_page(page_num):
-        if page_num == 1:
-            return col_main_section.find_all("li", class_="poster-container")
-        else:
-            page_url = f"{url}/page/{page_num}"
-            page_soup = BeautifulSoup(requests.get(page_url).text, "lxml")
-            page_body = page_soup.find("body")
-            page_col_main_section = page_body.find("section", class_="col-main")
-            return page_col_main_section.find_all("li", class_="poster-container")
+        num_pages = int(num_pages_tag.text.strip()) if num_pages_tag else 1
+        urls = [base_url] + [f"{base_url}/page/{page_num}"
+                             for page_num in range(2, num_pages + 1)]
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = executor.map(_fetch_page, range(num_pages + 1))
+        # Fetch all pages concurrently
+        pages = await asyncio.gather(*(_fetch_page(session, url) for url in urls))
 
-    containers = [container for result in results for container in result]
+        film_data = []
+        for page in pages:
+            first_page = BeautifulSoup(page, "lxml")
+            containers = first_page.find_all("li", class_="poster-container")
+            for container in containers:
+                film_name = container.select_one("div.linked-film-poster")["data-film-slug"]
+                liked = container.select_one("span.like") is not None
 
-    film_data = []
-    for container in containers:
-        film_name = container.select_one("div.linked-film-poster")["data-film-slug"]
-        liked = container.select_one("span.like") is not None
+                rating = None
+                if rating_tag := container.find("span", class_="rating"):
+                    rating_class = rating_tag["class"]
+                    rating = next((c for c in rating_class
+                                   if c.startswith("rated-")), None)
+                    rating = rating.split("-")[1] if rating else None
 
-        if rating_tag := container.find("span", class_="rating"):
-            rating_class = rating_tag["class"]
-            rating = next((c for c in rating_class if c.startswith("rated-")), None)
-            rating = rating.split("-")[1] if rating else None
-        else:
-            rating = None
+                film_data.append({
+                    "film_name": film_name,
+                    "liked": liked,
+                    "rating": rating
+                })
 
-        film_data.append({
-            "film_name": film_name,
-            "liked": liked,
-            "rating": rating
-        })
-
-    return film_data
+        return film_data
