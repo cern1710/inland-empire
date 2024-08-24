eda41c64ddb0c07c556be32552f237a9be8e0f1c
Samuel Wu <cernunnos1710@gmail.com>
2024-08-24 20:57:47 +0100

    Use select_one to target CSS elements

--- backend/scrape_user_ratings.py
+++ backend/scrape_user_ratings.py
@@ -6,15 +6,11 @@ def scrape_user_ratings(username: str):
     soup = BeautifulSoup(requests.get(url.format(username)).text, "lxml")
     body = soup.find("body")
 
-    profile_header = body.find("section", attrs={"class": "profile-header"})
+    profile_header_section = body.find("section", attrs={"class": "profile-header"})
 
-    display_name = (
-        profile_header.find("h1", attrs={"class": "title-3"})
-        .text.strip()
-    )
-    avatar_link = (
-        profile_header.find("a", attrs={"class": "avatar -a24"})
-        .find("img")["src"].strip()
-    )
+    display_name = profile_header_section.select_one("h1.title-3").text.strip()
+    avatar_link = profile_header_section.select_one("a.avatar.-a24 img")["src"].strip()
+
+    print(f"Display name: {display_name}, avatar link: {avatar_link}")
 
 scrape_user_ratings("mscorsese")
