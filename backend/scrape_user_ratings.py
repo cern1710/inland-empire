f59cd8e835a7074e0cc8234794ffd6266c377c9a
Samuel Wu <cernunnos1710@gmail.com>
2024-08-24 19:21:16 +0100

    Successfully parsed display name and user's avatar link

--- backend/scrape_user_ratings.py
+++ backend/scrape_user_ratings.py
@@ -5,6 +5,16 @@ def scrape_user_ratings(username: str):
     url = "https://letterboxd.com/{}/films/by/date"
     soup = BeautifulSoup(requests.get(url.format(username)).text, "lxml")
     body = soup.find("body")
-    print(body)
+
+    profile_header = body.find("section", attrs={"class": "profile-header"})
+
+    display_name = (
+        profile_header.find("h1", attrs={"class": "title-3"})
+        .text.strip()
+    )
+    avatar_link = (
+        profile_header.find("a", attrs={"class": "avatar -a24"})
+        .find("img")["src"].strip()
+    )
 
 scrape_user_ratings("mscorsese")
