b553ff20499d0f77d6fdf071c1348683d2378548
Samuel Wu <cernunnos1710@gmail.com>
2024-08-26 19:53:51 +0100

    Apply changes to __init__ with added scrape_movies function

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -1,7 +1,9 @@
 from .fetch_tmdb_data import get_tmdb_data
 from .scrape_user_ratings import scrape_user_ratings
+from .scrape_movie_data import scrape_movies
 
 __all__ = [
     'get_tmdb_data',
-    'scrape_user_ratings'
+    'scrape_user_ratings',
+    'scrape_movies'
 ]
