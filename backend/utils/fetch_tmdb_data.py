513b5ec7ac6911dc04456dff12c1f8a15e25e8fd
Samuel Wu <cernunnos1710@gmail.com>
2024-08-26 19:12:32 +0100

    Fetch directors from TMDB API for extra data in recommender system

--- backend/utils/fetch_tmdb_data.py
+++ backend/utils/fetch_tmdb_data.py
@@ -1,14 +1,14 @@
 import json
-from tmdbv3api import TMDb, Movie, Genre
+from tmdbv3api import TMDb, Movie
 from typing import Dict, Any, Tuple
 
 DEFAULT_CONFIG_PATH = "../config.json"
 
-def init_tmdb() -> Tuple[TMDb, Movie, Genre]:
+def init_tmdb() -> Tuple[TMDb, Movie]:
     tmdb = TMDb()
     tmdb.api_key = load_config()['tmdb']['api_key']
-    movie, genre = Movie(), Genre
-    return tmdb, movie, genre
+    movie = Movie()
+    return tmdb, movie
 
 def load_config(config_path: str = DEFAULT_CONFIG_PATH) -> dict:
     with open(config_path, 'r') as file:
@@ -16,12 +16,19 @@ def load_config(config_path: str = DEFAULT_CONFIG_PATH) -> dict:
 
 def get_tmdb_data(film_name: str) -> Dict[str, Any]:
     """Get movie information from TMDB based on film name."""
-    tmdb, movie, genre = init_tmdb()
+    tmdb, movie = init_tmdb()
     search = movie.search(film_name)
-    if search:
+
+    if len(search.results) > 0:
+        tmdb_id = search[0].id
+        credits = movie.credits(tmdb_id)
+        directors = [crew['id'] for crew in credits.get('crew', [])
+                     if crew['job'] == 'Director']
+
         tmdb_data = {
-            'tmdb_id': search[0].id,
+            'tmdb_id': tmdb_id,
             'title': search[0].title,
+            'directors': directors,
             'original_language': search[0].original_language,
             'genre_ids': search[0].genre_ids,
             'release_date': search[0].release_date,
