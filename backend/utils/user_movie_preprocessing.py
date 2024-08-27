95cf8f502ca9ada544c0084dbec4b2545d29054b
Samuel Wu <cernunnos1710@gmail.com>
2024-08-27 18:18:47 +0100

    get_user_movie_data: Scrapes user ratings based on their username, then scrapes data from each movie; returns a dictionary with tmdb_id as key.

---
+++ backend/utils/user_movie_preprocessing.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import asyncio
+import csv
+
+SLEEP_INTERVAL = 0.5
+CHUNK_SIZE = 200
+
+# Add parent directory to Python path
+PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(PARENT_DIR)
+
+from utils import scrape_user_ratings, scrape_movies
+
+async def get_user_movie_data(username: str):
+    film_slugs = await scrape_user_ratings(username)
+    print(f"Found {len(film_slugs)} films for {username}!")
+
+    chunks = [film_slugs[i : i + CHUNK_SIZE]
+              for i in range(0, len(film_slugs), CHUNK_SIZE)]
+    chunk_len = len(chunks)
+
+    user_movie_data = []
+    for i, chunk in enumerate(chunks):
+        print(f"Processing chunk {i+1} of {chunk_len}")
+
+        # TODO: what if another user's data is already in the database?
+        chunk_data = await scrape_movies(chunk, username)
+        user_movie_data.extend(chunk_data)
+        if i < chunk_len - 1:
+            await asyncio.sleep(SLEEP_INTERVAL)
+
+    return user_movie_data
