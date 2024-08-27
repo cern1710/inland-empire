2612ddba35653631db227a831f96e48785b16862
Samuel Wu <cernunnos1710@gmail.com>
2024-08-27 15:55:22 +0100

    Updated the lbxd movie scraper to scrape title, directors, genres, release year, rating info and runtime.

--- backend/tests/test_lbxd_scraper.py
+++ backend/tests/test_lbxd_scraper.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils import *
 
 if __name__ == "__main__":
-    movies = ['inland-empire', 'satantango', 'sicily']
+    movies = [{'film_slug':'inland-empire'}, {'film_slug':'satantango'},
+              {'film_slug':'sicily'}]
     info = asyncio.run(scrape_movies(movies))
     print(info)
